b39404912600589a48b8da0fc5ddc9299fa3a7a8
Torey <toreyaprice@gmail.com>
2025-03-05 10:40:17 -0600

    Update charts.js

--- charts.js
+++ charts.js
@@ -1,15 +1,16 @@
+window.onload = function() {
+  var data = JSON.parse(document.getElementById("graph-data").textContent);
 
-var data = JSON.parse(document.getElementById("graph-data").textContent);
-
-Plotly.newPlot(document.getElementById('tester'), 
-  [{x: data.g1.x1,y: data.g1.y1,type: 'line'},
-  {x: data.g1.x1,y: data.g1.y2,type: 'line'}], 
-  {margin: { t: 0 }},
-  {displayModeBar: false}
-  );
-Plotly.newPlot(document.getElementById('tester2'), 
-  [{x: data.g2.x1,y: data.g2.y1,type: 'line'},
-  {x: data.g2.x1,y: data.g2.y2,type: 'line'}], 
-  {margin: { t: 0 }},
-  {displayModeBar: false}
-  );
+  Plotly.newPlot(document.getElementById('tester'), 
+    [{x: data.g1.x1,y: data.g1.y1,type: 'line'},
+    {x: data.g1.x1,y: data.g1.y2,type: 'line'}], 
+    {margin: { t: 0 }},
+    {displayModeBar: false}
+    );
+  Plotly.newPlot(document.getElementById('tester2'), 
+    [{x: data.g2.x1,y: data.g2.y1,type: 'line'},
+    {x: data.g2.x1,y: data.g2.y2,type: 'line'}], 
+    {margin: { t: 0 }},
+    {displayModeBar: false}
+    );
+}
