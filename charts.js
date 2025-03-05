61ae28d7dfe2281ef7fc6b26f2f8a9f33d68e3a0
Torey <toreyaprice@gmail.com>
2025-03-05 11:00:20 -0600

    Update charts.js

--- charts.js
+++ charts.js
@@ -1,13 +1,18 @@
 window.onload = function() {
   var data = JSON.parse(document.getElementById("graph-data").textContent);
+  var div = JSON.parse(document.getElementById("graph-div").textContent);
+  div.insertAdjacentHTML('beforeend', `
+    <div id="chart-1" style="width:600px;height:250px;display:inline-block;"></div>
+    <div id="chart-2" style="width:600px;height:250px;display:inline-block;"></div>
+    `)
 
-  Plotly.newPlot(document.getElementById('tester'), 
+  Plotly.newPlot(document.getElementById('chart-1'), 
     [{x: data.g1.x1,y: data.g1.y1,type: 'line'},
     {x: data.g1.x1,y: data.g1.y2,type: 'line'}], 
     {margin: { t: 0 }},
     {displayModeBar: false}
     );
-  Plotly.newPlot(document.getElementById('tester2'), 
+  Plotly.newPlot(document.getElementById('chart-2'), 
     [{x: data.g2.x1,y: data.g2.y1,type: 'line'},
     {x: data.g2.x1,y: data.g2.y2,type: 'line'}], 
     {margin: { t: 0 }},
